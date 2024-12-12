e4567a4cb34d9030809f2010238951d7d503c857
laludonisetiawan <laludonisetiawan@gmail.com>
2024-12-12 23:54:10 +0700

    Menambahkan Product Service pada aplikasi, dan implementasi ke product component HTML

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import {ProductService} from './product.service';
 
 @Component({
   selector: 'app-product',
   standalone: false,
-  
+
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
 
+export class ProductComponent {
+  private productService: any;
+  productName : string = '';
+
+  constructor() {
+    const service = new ProductService();
+    this.productService = service;
+    this.productName = this.productService.productName;
+  }
 }
